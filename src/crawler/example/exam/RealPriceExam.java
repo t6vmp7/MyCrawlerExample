b227960aac9e411347de769c7bec153449188f97
t6vmp7 <t6vmp7@gmail.com>
2017-09-02 15:13:43 +0800

    爬蟲練習, 不動產實價登錄--高雄

--- src/crawler/example/exam/RealPriceExam.java
+++ src/crawler/example/exam/RealPriceExam.java
@@ -22,8 +22,7 @@ import org.jsoup.nodes.Element;
 public class RealPriceExam {
 	public static void main(String[] args) {
 		
-		String uri = "zip:http://plvr.land.moi.gov.tw"
-				+ "/Download?type=zip&fileName=lvr_landxml.zip"
+		String uri = "zip:http://plvr.land.moi.gov.tw//Download?type=zip&fileName=lvr_landxml.zip"
 				+ "!/A_LVR_LAND_A.XML";
 
 
